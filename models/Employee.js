61d4698eca4c551d9674b09a01c23f54aae21e57
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-22 19:17:42 -0400

    first and last name

--- models/Employee.js
+++ models/Employee.js
@@ -10,7 +10,11 @@ Employee.init(
             primaryKey: true,
             autoIncrement: true
         },
-        name: {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
             type: DataTypes.STRING,
             allowNull: false
         },
