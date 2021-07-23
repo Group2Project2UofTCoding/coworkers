badd5c234adb12e84a4cb682c4c37a0520b98083
PravTon <clintonsf@outlook.com>
2021-07-22 20:50:51 -0400

    updated the server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// creating the sess object
+const sess = {
+  secret: 'dash-coworker-secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({db: sequelize})
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+// turn on connection to DB
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listning on PORT ${PORT}`));
+});
