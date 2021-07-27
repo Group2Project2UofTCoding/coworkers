6d74d41375b0fcc619bc7eabd2c97ebb197791c5
Jaspreet Khela <jaspreetkhela@Jaspreets-MacBook-Pro.local>
2021-07-27 08:01:46 -0400

    Added event listeners for the dashboard page's buttons and finished the function for handling modal form submission

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -2,15 +2,19 @@
 // DOM Element Variables
 // __________
 
-// Employees' Tiles Section
-const employeeTiles = document.getElementById("");
+// Employees' tiles section
+const employeeTiles = document.getElementById("employeeTiles");
 
 // Buttons
 const addEmployeeButton = document.getElementById("addEmployeeButton");
 const editEmployeeButton = document.getElementById("editEmployeeButton");
 const deleteEmployeeButton = document.getElementById("removeEmployeeButton");
 
-// Add/Edit Employee's Modal Inputs
+// Forms
+const addEditEmployeeForm = document.getElementById("addEditEmployeeModal");
+const removeEmployeeForm = document.getElementById("removeEmployeeModal");
+
+// Add/Edit employee's modal inputs
 const employeeFirstName = document.getElementById("employeeFirstName");
 const employeeLastName = document.getElementById("employeeLastName");
 const employeeUsername = document.getElementById("employeeUsername");
@@ -30,7 +34,7 @@ const employeeEmergencyContactPhoneNumber = document.getElementById("employeeEme
 const employeeCertification = document.getElementById("employeeCertification");
 const employeeSubmit = document.getElementById("addEditEmployeeSubmit");
 
-// Delete Employee's Modal Inputs
+// Delete employee's modal inputs
 const selectedEmployee = document.getElementById("selectedEmployee");
 const deleteEmployeeSubmit = document.getElementById("removeEmployeeSubmit");
 
@@ -42,82 +46,94 @@ const deleteEmployeeSubmit = document.getElementById("removeEmployeeSubmit");
 // Functions
 // __________
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+// Retrieve all employees' information
+function employeesInformation() {
+  
+}
 
-// Handle Viewing an Employee's Information
-function handleViewEmployeeModal(event) {
-    // Prevent default event behaviours
-    event.preventDefault();
+// Populate the dashboard with current employees' information
+function generateEmployeeTiles(employeesObject) {
+  console.log(employeesObject);
 
-    var employeeID = event.target.value;
+  const employeeHTML = employeesObject.map(());
 
+  employeeTiles.innerHTML = employeeHTML.join('');
+}
 
+// Handle viewing an employee's information
+function handleEmployeeModalView(event) {
+  // Prevent default event behaviours
+  event.preventDefault();
 
+  var employeeID = event.target.value;
 
 }
 
-// Handle Modal Form Data
+// Handle modal form data
 function handleEmployeeModalSubmit(event) {
-    // Prevent default event behaviours
-    event.preventDefault();
+  // Prevent default event behaviours
+  event.preventDefault();
 
-    // Store information from the modal upon submit
-    var name = employeeName.value;
-    var role = employeeRole.value;
-    var level = employeeLevel.value;
-    var department = employeeDepartment.value;
-    var salary = employeeSalary.value;
-    var address = employeeAddress.value;
-    var email = employeeEmail.value;
-    var phone = employeePhone.value;
-    var sin = employeeSIN.value;
-    var emergencyContact = employeeEmergencyContact.value;
-    var emergencyContactNumber = employeeEmergencyContactNumber.value;
+  // Store information from the modal upon submit
+  const firstName = employeeFirstName.value;
+  const lastName = employeeLastName.value;
+  const username = employeeUsername.value;
+  const email = employeeEmail.value;
+  const password = employeePassword.value;
+  const phoneNumber = employeePhoneNumber.value;
+  const addressLine1 = employeeAddress1.value;
+  const addressLine2 = employeeAddress2.value;
+  const city = employeeCity.value;
+  const role = employeeRole.value;
+  const level = employeeLevel.value;
+  const department = employeeDepartment.value;
+  const salary = employeeSalary.value;
+  const sin = employeeSIN.value;
+  const contactName = employeeEmergencyContactName.value;
+  const contactNumber = employeeEmergencyContactPhoneNumber.value;
+  const certification = employeeCertification.value;
 
-    var employeeObject = { name, role, level, department, salary, address, email, phone, sin, phone, emergencyContact, emergencyContactNumber };
+  var employeeObject = { firstName, lastName, username, email, password, phoneNumber, addressLine1, addressLine2, city, role, level, department, salary, sin, contactName, contactNumber, certification };
 
-    fetch('api/employee', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(employeeObject)
+  console.log(employeeObject);
+  
+  fetch('api/employee', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(employeeObject)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        alert('Error: ' + response.statusText);
       })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          alert('Error: ' + response.statusText);
-        })
-        .then(postResponse => {
-          console.log(postResponse);
-          alert('Thank you for adding an employee');
-        });
-    };
+      .then(postResponse => {
+        console.log(postResponse);
+        alert('Thank you for adding an employee');
+      });
+}
+
+// Handle removing an employee's information
+function handleEmployeeRemoveSubmit(event) {
+
 }
 
 // __________
 // Event Handlers
 // __________
 
+// Event listener for submiting an a new employee
+addEditEmployeeForm.addEventListener("submit", handleEmployeeModalSubmit);
+
+// Event listener for viewing an employee's information in a modal
+editEmployeeButton.addEventListener("click", handleEmployeeModalView);
+
+// Event listener for removing an employee
+removeEmployeeForm.addEventListener("submit", handleEmployeeRemoveSubmit);
 
 // __________
 // Script
