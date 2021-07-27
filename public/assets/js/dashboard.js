2547b0092c83f865cd1f835441be9a143b4609dd
Jaspreet Khela <jaspreetkhela@Jaspreets-MacBook-Pro.local>
2021-07-26 20:42:48 -0400

    Added DOM element variables for the employee modal

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -2,6 +2,22 @@
 // DOM Element Variables
 // __________
 
+//Modal
+const employeeForm = document.getElementById("");
+
+// Employee's Modal Inputs
+const employeeName = document.getElementById("");
+const employeeRole = document.getElementById("");
+const employeeLevel = document.getElementById("");
+const employeeDepartment = document.getElementById("");
+const employeeSalary = document.getElementById("");
+const employeeAddress = document.getElementById("");
+const employeeEmail = document.getElementById("");
+const employeePhone = document.getElementById("");
+const employeeSIN = document.getElementById("");
+const employeeContact = document.getElementById("");
+const employeeContactNumber = document.getElementById("");
+
 // __________
 // Non-DOM Element Variables
 // __________
