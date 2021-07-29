b623463fe86ff5caedaf8a6836bbb98fb47ac49f
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-29 13:32:56 -0400

    email validation on manager put/post

--- controllers/api/manager-routes.js
+++ controllers/api/manager-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { Employee, Manager } = require('../../models');
+const validator = require('validator');
 
 // Get all managers
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   Manager.findAll({
     attributes: {
       exclude: ['password'],
@@ -15,58 +16,60 @@ router.get('/', (req,res) => {
       }
     ]
   })
-  .then(dbManagerData => res.json(dbManagerData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  })
+    .then(dbManagerData => res.json(dbManagerData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 
 // Get One Manager
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res) => {
   Manager.findOne({
     where: {
       id: req.params.id
-    },  
-    attributes: {exclude: ['password']},
+    },
+    attributes: { exclude: ['password'] },
     // include: [
     //   {
     //     model: Employee
     //   }
     // ]
   })
-  .then(dbManagerData => res.json(dbManagerData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  })
+    .then(dbManagerData => res.json(dbManagerData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // Update a Manager
 router.put('/:id', (req, res) => {
-  Manager.update({
-    id: req.body.managerId,
-    email: req.body.email,
-    password: req.body.password,
-  }, 
-  {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }  
-  })
-  .then(dbManagerData => {
-    if(!dbManagerData) {
-      res.sendStatus(404).json({ message: 'Manager not found! Please check the id'})
-      return;
-    }
-    res.json(dbManagerData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+  if (validator.isEmail(req.body.email)) {
+    Manager.update({
+      id: req.body.managerId,
+      email: req.body.email,
+      password: req.body.password,
+    },
+      {
+        individualHooks: true,
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(dbManagerData => {
+        if (!dbManagerData) {
+          res.sendStatus(404).json({ message: 'Manager not found! Please check the id' })
+          return;
+        }
+        res.json(dbManagerData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Delete a Manager
@@ -76,83 +79,87 @@ router.delete('/:id', (req, res) => {
       id: req.params.id
     }
   })
-  .then(dbManagerData => {
-    if(!dbManagerData) {
-      res.sendStatus(404).json({ message: 'Manager not found! Please check the id'});
-      return;
-    }
-    res.json(dbManagerData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbManagerData => {
+      if (!dbManagerData) {
+        res.sendStatus(404).json({ message: 'Manager not found! Please check the id' });
+        return;
+      }
+      res.json(dbManagerData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // add a manager
 router.post('/', (req, res) => {
-  Manager.create({
-    // employee_id: req.body.employee_id,
-    id: req.body.managerId,
-    email: req.body.email,
-    password: req.body.password
-  })
-  .then(dbManagerData => {
-    req.session.save(() => {
-      // declare session variables
-      req.session.manager_id = dbManagerData.id;
-      req.session.email = dbManagerData.email;
-      req.session.loggedIn = true;
-      // send response
-      res.json({ user: dbManagerData, message: 'You are now logged in!' });
-      console.log(req.session);
-      res.json(dbManagerData);
+  if (validator.isEmail(req.body.email)) {
+    Manager.create({
+      // employee_id: req.body.employee_id,
+      id: req.body.managerId,
+      email: req.body.email,
+      password: req.body.password
     })
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+      .then(dbManagerData => {
+        req.session.save(() => {
+          // declare session variables
+          req.session.manager_id = dbManagerData.id;
+          req.session.email = dbManagerData.email;
+          req.session.loggedIn = true;
+          // send response
+          res.json({ user: dbManagerData, message: 'You are now logged in!' });
+          console.log(req.session);
+          res.json(dbManagerData);
+        })
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Login route for Manager
 router.post('/login', (req, res) => {
   // {email: 'user@email.com', password: 'password' }
-  Manager.findOne({
-    where: {
-      email: req.body.email
-    }
-  }).then(dbManagerData => {
-    if(!dbManagerData) {
-      res.status(400).json({message: 'No user with that email address!'})
-      return;
-    }
-    
-    //authenticate the user
-    const validPassword = dbManagerData.checkPassword(req.body.password);
-    if(!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+  if (validator.isEmail(req.body.email) == true && validator.isStrongPassword(req.body.password, [{ minLength: 8 }]) == true) {
+    Manager.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbManagerData => {
+      if (!dbManagerData) {
+        res.status(400).json({ message: 'No user with that email address!' })
+        return;
+      }
 
-    req.session.save(() => {
-      // declare session variables
-      req.session.manager_id = dbManagerData.id;
-      req.session.email = dbManagerData.email;
-      req.session.loggedIn = true;
-      // send response
-      res.json({ user: dbManagerData, message: 'You are now logged in!' });
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+      //authenticate the user
+      const validPassword = dbManagerData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+
+      req.session.save(() => {
+        // declare session variables
+        req.session.manager_id = dbManagerData.id;
+        req.session.email = dbManagerData.email;
+        req.session.loggedIn = true;
+        // send response
+        res.json({ user: dbManagerData, message: 'You are now logged in!' });
+      });
+    })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Logout route for manager
 router.post('/logout', (req, res) => {
-  if(req.session.loggedIn) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
