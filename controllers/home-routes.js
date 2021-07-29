f05aa4a31c807fb5635fe95fe745c672ccafd02c
PravTon <clintonsf@outlook.com>
2021-07-29 02:49:35 -0400

    updated the hero tabs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 router.get('/',(req,res) => {
   if(req.session.loggedIn) {
-    res.redirect('/dasboard');
+    res.redirect('/dashboard');
     return;
   }
 
