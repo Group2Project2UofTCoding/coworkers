0dd3da123724180e94e0e7c648410a4d55dcc0c9
PravTon <clintonsf@outlook.com>
2021-07-29 13:53:51 -0400

    added a chart in dashboard

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -1,35 +1,41 @@
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var myChart = new Chart(ctx, {
-//   type: 'line',
-//   data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//           label: '# of Votes',
-//           data: [12, 19, 3, 5, 2, 3],
-//           backgroundColor: [
-//               'rgba(255, 99, 132, 0.2)',
-//               'rgba(54, 162, 235, 0.2)',
-//               'rgba(255, 206, 86, 0.2)',
-//               'rgba(75, 192, 192, 0.2)',
-//               'rgba(153, 102, 255, 0.2)',
-//               'rgba(255, 159, 64, 0.2)'
-//           ],
-//           borderColor: [
-//               'rgba(255, 99, 132, 1)',
-//               'rgba(54, 162, 235, 1)',
-//               'rgba(255, 206, 86, 1)',
-//               'rgba(75, 192, 192, 1)',
-//               'rgba(153, 102, 255, 1)',
-//               'rgba(255, 159, 64, 1)'
-//           ],
-//           borderWidth: 1
-//       }]
-//   },
-//   options: {
-//       scales: {
-//           y: {
-//               beginAtZero: true
-//           }
-//       }
-//   }
-// });
+const labels = [
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July'
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Monthly Expenses',
+    backgroundColor: '#E56F6B',
+    borderColor: '#E56F6B',
+    data: [10000, 13000, 15000, 12000, 8000, 17000, 20000],
+  },
+  {
+    label: 'Monthly Salary',
+    backgroundColor: '#477998',
+    borderColor: '#477998',
+    data: [20000, 20000, 25000, 25000, 30000, 40000, 35000],
+  },
+  {
+    label: 'Sales',
+    backgroundColor: '#48A9A6',
+    borderColor: '#48A9A6',
+    data: [40000, 50000, 60000, 55000, 80000, 60000, 75000],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data,
+  options: {}
+};
+
+var myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
